ae7893ac0733c9f93c7a6b43a94c7822bc1c1608
Amir JCS <amirmahdyjdx@gmail.com>
2024-10-28 23:32:20 +0330

    fix: deletion of receipts and expenses with the amount of 1

--- core/main.go
+++ core/main.go
@@ -12,19 +12,34 @@ func gcf(u, v int) int {
 	return gcf(v, u%v)
 }
 
-func toRadian() js.Func {
+func SPrintFractionStyleWithPI(on, under int, onVar string) string {
+	onStr := fmt.Sprintf("%d%v", on, onVar)
+	if on == 1 {
+		onStr = onVar
+	}
+	underStr := fmt.Sprintf("%d", under)
+	if under != 1 {
+		underStr = fmt.Sprintf("\\over %v}", underStr)
+	} else {
+		underStr = "}"
+	}
+
+	return fmt.Sprintf("{%v %v", onStr, underStr)
+}
+
+func toJsRadian() js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) any {
 		if len(args) != 1 {
 			return "Invalid no of arguments passed"
 		}
 		var degree int = args[0].Int()
 		n := gcf(180, degree)
-		return fmt.Sprintf("\\frac{%v\\pi}{%v}", degree/n, 180/n)
+		return SPrintFractionStyleWithPI((degree / n), (180 / n), "\\pi")
 	})
 }
 
 func main() {
 	fmt.Println("Code agha")
-	js.Global().Set("toRadian", toRadian())
+	js.Global().Set("toRadian", toJsRadian())
 	<-make(chan struct{})
 }
